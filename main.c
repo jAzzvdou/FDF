99c49bbfc17df1a218c6d5a178c56be40755e673
jAzzvdou <joaoazevedo592@gmail.com>
2024-03-20 23:45:35 -0300

    Update: Reading Map..(?)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jazevedo <jazevedo@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 17:33:34 by jazevedo          #+#    #+#             */
-/*   Updated: 2024/03/20 16:52:50 by jazevedo         ###   ########.fr       */
+/*   Updated: 2024/03/20 23:02:33 by jazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,32 @@ int	map_size(int fd)
 	return (1);
 }
 
+t_coords	read_map(char *file)
+{
+	int	fd;
+	char	*line;
+	t_coords	map;
+
+	fd = open(file, O_RDONLY);
+	map = (t_coords){0};
+	map.x = 0;
+	map.y = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		map.y++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	map.x = count_spaces(line) + 1;
+	free(line);
+	close(fd);
+	return (map);
+}
+
 int	main(int argc, char **argv)
 {
 	int	fd;
@@ -67,5 +93,12 @@ int	main(int argc, char **argv)
 		return (write(2, "Error! Invalid File.\n", 21));
 	if (!map_size(fd))
 		return (write(2, "Error! Invalid Map Size.\n", 25));
+	// Now I need to read the map and put de coords in a 2D array.
+	fdf.map = read_map(argv[1]);
+	// Then I need to get the max and min values of the map.
+	// Then I need to convert the 2D array to a 3D.
+	// Then I need to draw the map.
+	// Then I need to handle the events.
+	// Then I need to free the memory.
 	return (0);
 }
